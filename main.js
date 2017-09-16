397f2319f4784f0e9c50cfaa741908bd12db488e
Stanley <snicholl@gmail.com>
2017-09-16 10:00:05 -0700

    canvas complete - will need mods for 13 inch screen

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 document.addEventListener('DOMContentLoaded', function(){
   var createGrid = function(){
-    let pixel = document.createElement('div')
     let canvas = document.getElementById('canvas')
-    pixel.className= "pixel"
-    for(let i = 0; i < 7000; i++){
+    for(let i = 0; i < 4851; i++){
+      let pixel = document.createElement('div')
+      pixel.className= "pixel"
       canvas.appendChild(pixel)
     }
   }
 
   createGrid()
+
+  var createColorTool = function(){
+    let colorSection1 = ["crimson", "coral", "orange", "chocolate", "gold", "darkgoldenrod", "sandybrown", "burlywood", "bisque", "peachpuff", "lightyellow", "yellow", "greenyellow", "lime"]
+    let colorSection2 = ["seagreen", "forestgreen", "darkgreen", "navy", "blue", "royalblue", "cornflowerblue", "blueviolet", "deeppink", "indigo", "fuchsia"]
+    let colorSection3 = ["black", "dimgray", "gray", "darkgray", "Gainsboro", "white"]
+    var allColors = colorSection1.concat(colorSection2, colorSection3)
+    let paintSection1 = document.getElementById('paint-section-1')
+    for(let i = 0; i < allColors.length; i++){
+      let tool = document.createElement('div')
+      tool.className = 'color-pick'
+      tool.style.backgroundColor = allColors[i]
+      paintSection1.appendChild(tool)
+    }
+  }
+
+  createColorTool()
 })
